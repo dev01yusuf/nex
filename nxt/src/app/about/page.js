ea34c383d9c4bf8ec182f1d0bc719e98e43e7d61
youssef taoufikalllah <46559835+dev01yusuf@users.noreply.github.com>
2023-05-05 12:57:28 +0100

    add project

---
+++ nxt/src/app/about/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from 'next/head'
+
+function about() {
+  return (
+    <div>
+      <Head>
+        <title>about nexs</title>
+        <meta name="keywords" content="web dev, prog" />
+      </Head>
+      <h1>about</h1>
+    </div>
+  );
+}
+
+export default about;
---
+++ nxt/src/app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+          <title>zeb nexs</title>
+          <meta name='keywords' content='web dev, prog' />
+      </Head>
+      <h1>welcome to next</h1>
+    </div>
+  )
+}
